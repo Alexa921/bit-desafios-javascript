4db6935e1e438203499c12a4a36934cd2fb5cfea
Alexa921 <alexandrarestrepoagudelo451@gmail.com>
2024-12-19 18:39:06 -0500

    Ultima actualizacion del proyecto

--- javascript/script1.js
+++ javascript/script1.js
@@ -1,8 +1,8 @@
-/*1. Crear una variable y asignarle su nombre, crea una constante y asignarle su edad, luegoen consola:
+/*1. Crear una variable y asignarle su nombre, crea una constante y asignarle su edad, luego en consola:
 Hola, soy ___ y tengo ___ años.*/
 
 let miNombre = "Alexandra";
-    const miEdad = 21;
-    console.log(`Hola, soy ${miNombre} y tengo ${miEdad} años`);
+const miEdad = 21;
+console.log(`Hola, soy ${miNombre} y tengo ${miEdad} años`);
 
 
--- javascript/script11.js
+++ javascript/script11.js
@@ -3,6 +3,9 @@
 function imprimirMayor(num1, num2) {
     if (num1 > num2) {
         console.log(num1);
+    } else {
+        console.log(num2);
     }
 }
 imprimirMayor(10, 5);
+imprimirMayor(5, 10);
