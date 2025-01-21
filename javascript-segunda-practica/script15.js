ad3c6e07d9818e21205b94f6b76a595905193604
Alexa921 <alexandrarestrepoagudelo451@gmail.com>
2025-01-20 21:32:37 -0500

    Nuevos ejercicios

---
+++ javascript-segunda-practica/script15.js
@@ -0,0 +1,4 @@
+/*16. Solicitar nombre y apellido al usuario y mostrarlos en una ventana emergente.*/
+
+const nombreApellido = prompt('Ingrese nombre y apellido.');
+console.log('nombreApellido');
---
+++ javascript-segunda-practica/script16.js
@@ -0,0 +1,8 @@
+/*17. Solicitar nombre y edad al usuario y mostrar en una ventana emergente el
+mensaje: "Hola, ... tienes ... años.""*/
+const nombre = prompt('Ingerese su nombre');
+console.log('nombre');
+const edad = prompt('Ingerese su edad');
+console.log('edad');
+const resultado = alert('Hola,' + ' ' + nombre + ' ' + 'tienes' + ' ' + edad);
+console.log(resultado);
