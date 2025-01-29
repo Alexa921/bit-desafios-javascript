ff7349e2b07f46607ef8d71381784e6735a48eb9
Alexa921 <alexandrarestrepoagudelo451@gmail.com>
2025-01-29 16:07:15 -0500

    Actualizacion

--- bit-desafios-dom/script1.js
+++
@@ -1,8 +0,0 @@
-/*1. HTML con únicamente un div, replicar la muestra de la manipulación del DOM
-explicada en clase, Debe mostrar su nombre completo con el texto a color.*/
-
-const elementoManipulacionDom = document.getElementById('manipulacion-dom');
-console.log(elementoManipulacionDom);
-
-elementoManipulacionDom.textContent = 'Alexandra Restrepo Agudelo';
-elementoManipulacionDom.className = 'nombre';
